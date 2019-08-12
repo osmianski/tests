35aecffc963118aa58f3476600b0690d0ee98fe2
Vladislav Osmianskij <vlaosm@gmail.com>
2019-08-12 16:58:39 +0300

    better elapsed time formatting

--- tests/lazy_properties.php
+++ tests/lazy_properties.php
@@ -5,25 +5,25 @@ use Osmianski\Tests\LazyProperties;
 include __DIR__ . '/../vendor/autoload.php';
 
 $object = new LazyProperties();
-$count = 1000;
+$count = 10000;
 
 $startedAt = microtime(true);
 for ($i = 0; $i < $count; $i++) {
     $value = $object->getFoo();
 }
-$elapsed = (microtime(true) - $startedAt) * 1000;
+$elapsed = sprintf("%.2f", (microtime(true) - $startedAt) * 1000);
 echo "'foo' property accessed {$count} times in {$elapsed}ms\n";
 
 $startedAt = microtime(true);
 for ($i = 0; $i < $count; $i++) {
     $value = $object->getBar();
 }
-$elapsed = (microtime(true) - $startedAt) * 1000;
+$elapsed = sprintf("%.2f", (microtime(true) - $startedAt) * 1000);
 echo "'bar' property accessed {$count} times in {$elapsed}ms\n";
 
 $startedAt = microtime(true);
 for ($i = 0; $i < $count; $i++) {
     $value = $object->magic;
 }
-$elapsed = (microtime(true) - $startedAt) * 1000;
+$elapsed = sprintf("%.2f", (microtime(true) - $startedAt) * 1000);
 echo "'magic' property accessed {$count} times in {$elapsed}ms\n";
